81c266dff5e579052d4b4a30105c5662da1a866c
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-07-27 16:18:07 -0300

    Create Provider in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import RepositoryList from './components/RepositoryList';
 
-function App() {
-  return <div className="App" />;
-}
+import store from './store';
+
+const App = () => (
+  <Provider store={store}>
+    <RepositoryList />
+  </Provider>
+);
 
 export default App;
--- src/components/RepositoryList/index.tsx
+++ src/components/RepositoryList/index.tsx
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
 
-export default class RepositoryList extends Component {
-  componentDidMount() {}
+import { Repository } from '../../store/ducks/repositories/types';
+import { ApplicationState } from '../../store';
+import * as repositoriesActions from '../../store/ducks/repositories/actions';
+
+interface StateProp {
+  repositories: Repository[];
+}
+
+interface DispatchProps {
+  loadRequest(): void;
+}
+
+type Props = StateProp & DispatchProps;
+
+class RepositoryList extends Component<Props> {
+  componentDidMount() {
+    const { loadRequest } = this.props;
+  }
 
   render() {
-    return <ul />;
+    const { repositories } = this.props;
+    return <ul>{repositories.map((repository) => repository.name)}</ul>;
   }
 }
+
+const mapStateToProps = (state: ApplicationState) => ({
+  repositories: state.repositories.data,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) =>
+  bindActionCreators(repositoriesActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(RepositoryList);
